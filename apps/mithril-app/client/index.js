aed786d252d4553a8040c2a2019673fc9b335151
Zackery Griesinger <zgriesinger@gmail.com>
2024-07-08 08:36:05 -0500

    Upgrade to pnpm 9

--- apps/mithril-app/client/index.js
+++
@@ -1,7 +0,0 @@
-// Babel has deprecated @babel/polyfill, and the following two imports are used for polyfills instead.
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import m from 'mithril';
-import { MainView } from './MainView';
-
-m.mount(document.body, MainView);
